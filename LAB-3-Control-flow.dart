8aad6804f531b1993e8430fa69ceb484ef64e8b3
UGR/2898/13 <147318783+abrshworke@users.noreply.github.com>
2024-03-08 16:46:09 +0300

    LAB-3-Control-flow

---
+++ LAB-3-Control-flow.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter the first number: ");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter the second number: ");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Choose an operation (+, -, *, /): ");
+  String operation = stdin.readLineSync()!;
+
+  double result;
+
+  switch (operation) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      if (num2 != 0) {
+        result = num1 / num2;
+      } else {
+        print("Error: Division by zero.");
+        return;
+      }
+      break;
+    default:
+      print("Error: Invalid operation.");
+      return;
+  }
+
+  print("Result: $result");
+}
