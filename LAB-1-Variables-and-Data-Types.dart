ca7da35ad20a8c68b543ab75ac7a31e942299f52
UGR/2898/13 <147318783+abrshworke@users.noreply.github.com>
2024-03-08 16:00:08 +0300

    Delete LAB-1-Variables-and-Data-Types.dart

--- LAB-1-Variables-and-Data-Types.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  String name = "Abreham Worke";
-  int age = 22;
-  String favoriteColor = "green";
-  print(
-      "My name is $name, I am $age years old, and also my favorite color is $favoriteColor.");
-}
