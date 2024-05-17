b1741d6d316365ce82e3e2dd4f73729024b52072
UGR/2898/13 <147318783+abrshworke@users.noreply.github.com>
2024-05-17 16:15:19 +0300

    kotlin-exercise

---
+++ kotlin_exercise/exercise1/ex-1.kt
@@ -0,0 +1,13 @@
+fun main() {
+    
+    val currentTime: String = "12:30 AM"
+
+   
+    val temperature: Double = 72.5
+
+    val grade: Char = 'A'
+
+    println("Current Time: $currentTime")
+    println("Temperature: $temperature°F")
+    println("Grade: $grade")
+}
